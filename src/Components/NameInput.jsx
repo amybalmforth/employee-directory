9050f1be4af8f37809e24c40fd0ce2bbe089e796
Amy Balmforth <amy.balmforth@gmail.com>
2021-02-11 20:29:00 +0000

    input field for entering a new name and binding to nameinput state

--- src/Components/NameInput.jsx
+++ src/Components/NameInput.jsx
@@ -2,12 +2,25 @@ import React, {Component} from 'react';
 
 class NameInput extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {newName: ''};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({newName: event.target.value});
+  }
+
   render() {
     return (
-        <>
-        Input component here
-        </>
-
+      <div>
+        <form>
+          <input type="text"
+                  placeholder="Enter a name"
+                  onChange={this.handleChange} value={this.state.newName}/>
+        </form>
+      </div>
     )
   }
 
