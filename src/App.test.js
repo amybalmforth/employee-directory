cbd1ca33f07806f85bf52346f3b8dc476b65b24f
Amy Balmforth <amy.balmforth@gmail.com>
2021-02-13 20:15:09 +0000

    tests added for input, submit and delete a name

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('title is visible', () => {
   render(<App />);
-  const linkElement = screen.getByText('Component here');
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText('Employees');
+  expect(title).toBeInTheDocument();
+});
+
+test('name input is visible', () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText('Employee name');
+  expect(input).toBeInTheDocument();
+});
+
+test('email input is visible', () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText('Employee email');
+  expect(input).toBeInTheDocument();
+});
+
+test('can enter a name and email and submit', () => {
+  render(<App />);
+  fireEvent.change(screen.getByPlaceholderText('Employee name'), {
+      target: { value: 'New Person' },
+  });
+  fireEvent.change(screen.getByPlaceholderText('Employee email'), {
+      target: { value: 'new@person.com' },
+  });
+  fireEvent.click(screen.getByText('Submit'))
+  const person = screen.getByText('New Person - new@person.com');
+  expect(person).toBeInTheDocument();
+});
+
+test('can delete a name', () => {
+  const app = render(<App />);
+  fireEvent.change(screen.getByPlaceholderText('Employee name'), {
+      target: { value: 'Person to delete' },
+  });
+  fireEvent.change(screen.getByPlaceholderText('Employee email'), {
+      target: { value: 'person@delete.com' },
+  });
+  fireEvent.click(screen.getByText('Submit'))
+  const toDelete = app.container.querySelector('#delete');
+  const person = screen.getByText('Person to delete - person@delete.com');
+  fireEvent.click(toDelete)
+  expect(person).not.toBeInTheDocument();
 });
